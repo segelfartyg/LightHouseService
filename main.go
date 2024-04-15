9ce74a044e9adcac1c9c788b40b9a96a3530c973
segelfartyg <samuel.swaren@outlook.com>
2024-04-15 23:01:35 +0200

    Add: ws support

--- main.go
+++ main.go
@@ -2,19 +2,48 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
 )
 
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true },
+}
+
 var lightHouseIntervalSeconds = "1"
+var wsMessage = "nolight"
 
 func main() {
 	router := gin.Default()
 	router.GET("/ping", Ping)
-
 	router.GET("/interval", GetInterval)
-
+	router.GET("/ws", StartWebSocket)
 	router.Run(":3000")
+}
+
+func Timer() {
+	for {
+		wsMessage = "light"
+		time.Sleep(time.Second)
+		wsMessage = "nolight"
+	}
+}
+
+func StartWebSocket(c *gin.Context) {
+	conn, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+	if err != nil {
+		return
+	}
+	for {
+		conn.WriteMessage(websocket.TextMessage, []byte("light"))
+		time.Sleep(time.Second / 2)
+		conn.WriteMessage(websocket.TextMessage, []byte("lights"))
+		time.Sleep(time.Second / 2)
+	}
 }
 
 func Ping(c *gin.Context) {
